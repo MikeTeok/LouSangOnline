aca1e6cbab09f85d6e96b1194656dd2ad7452bfe
bingxuan3133 <bensoncbx@gmail.com>
2022-01-10 22:17:49 +0800

    makes player camera moves

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,9 +5,29 @@ var puppet_position = Vector2.ZERO
 onready var tween = $Tween
 onready var timer = $NetworkTickRate
 
+onready var camera = $Camera
+var mouseDelta : Vector2 = Vector2()
+var minLookAngle : float = -45.0
+var maxLookAngle : float = 45.0
+var lookSensitivity : float = 0.5
+
 func _physics_process(delta):
 	if is_network_master():
 		pass
+
+func _process (delta):
+	# rotate camera along X axis
+	camera.rotation_degrees -= Vector3(rad2deg(mouseDelta.y), 0, 0) * lookSensitivity * delta
+	# clamp the vertical camera rotation
+	camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, minLookAngle, maxLookAngle)
+	# rotate player along Y axis
+	rotation_degrees -= Vector3(0, rad2deg(mouseDelta.x), 0) * lookSensitivity * delta
+	# reset the mouse delta vector
+	mouseDelta = Vector2()
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		mouseDelta = event.relative
 
 puppet func update_state(p_position):
 	puppet_position = p_position
